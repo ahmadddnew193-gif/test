55288b40af4895e6754dd3a1d6cd213b17c8d2c6
ahmadddnew193-gif <ahmadddnew193@gmail.com>
2025-10-04 16:32:49 +0300

    initial

---
+++ new.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+if "player_count" not in st.session_state:
+    st.session_state.vis = []
+    st.session_state.plr = []
+
+st.set_page_config(page_title="Camera Test", layout="wide")
+st.title("📸 Camera & Audio Test")
+
+
+
+cam = st.camera_input("Take a photo")
+
+if cam:
+    st.success("Photo captured!")
+    st.download_button("Download Photo", data=cam.read(), file_name="photo.png")
